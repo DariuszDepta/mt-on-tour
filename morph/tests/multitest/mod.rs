ac658eb611d69ec73a97910deeb4d7c0cd8993b7
Dariusz Depta <141360751+DariuszDepta@users.noreply.github.com>
2025-05-08 11:17:34 +0200

    Added tests using MultiTest.

---
+++ morph/tests/multitest/mod.rs
@@ -0,0 +1,26 @@
+mod test_execute;
+mod test_instantiate;
+mod test_metadata;
+mod test_query;
+
+use cosmwasm_std::{Checksum, Empty};
+use cw_multi_test::{Contract, ContractWrapper};
+
+/// Checksum calculated from `morph.wasm` binary.
+///
+/// ```shell
+/// $ cargo build --release --lib --target wasm32-unknown-unknown
+/// $ sha256sum ../target/wasm32-unknown-unknown/release/morph.wasm | awk '{print toupper($1)}'
+/// ```
+const CHECKSUM: &str = "757EDD8B310480C81A2B0E79D3E9555070C1A4544BEACA920E7AD0DA1136951C";
+
+pub fn intro() -> Box<dyn Contract<Empty>> {
+    Box::new(
+        ContractWrapper::new_with_empty(
+            morph::contract::execute,
+            morph::contract::instantiate,
+            morph::contract::query,
+        )
+        .with_checksum(Checksum::from_hex(CHECKSUM).unwrap()),
+    )
+}
